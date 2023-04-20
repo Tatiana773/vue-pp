94f71bbc7cf552d449b47830f0c2a76caa20f180
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2023-04-20 21:06:48 +0300

    Forms

--- src/components/AddItem/AddItem.vue
+++ src/components/AddItem/AddItem.vue
@@ -1,142 +1,163 @@
 <template>
     <v-row justify="center">
-    <v-dialog
-        v-model="dialog"
-        persistent
-        max-width="500"
-    >
-        <template v-slot:activator="{ on, attrs }">
-        <v-btn
-            color="success"
-            dark
-            v-bind="attrs"
-            v-on="on"
+        <v-btn @click="onEdit">Edit</v-btn>
+        <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="500"
         >
-            Add Item
-        </v-btn>
-        </template>
-        <v-card class = "pa-6">
-            <v-text-field
-            label="Title"
-            outlined
-            dense
-            :value="titleValue"
-            @input="onTitleChange"
-            ></v-text-field>
-            <v-textarea
-            outlined
-            name="input-7-4"
-            label="Content"
-            :value="contentValue"
-            @input="onContentChange"
-            ></v-textarea>
-            <v-select
-            :items="items"
-            item-text="title" 
-            item-value="catId"
-            label="Category"
-            outlined
-            dense
-            :value="categoryValue"
-            @change="onCategoryChange"
-            ></v-select>
-            <v-card-actions>
-                <v-spacer></v-spacer>
+            <template v-slot:activator="{ on, attrs }">
                 <v-btn
-                color="purple darken-2"
-                text
-                @click="onBack"
-                >
-                <v-icon
-                dark
-                left
-                >
-                mdi-arrow-left
-                </v-icon>
-                Back
-                </v-btn>
-                <v-btn
-                color="primary"
-                text
-                @click="onAdd"
-                >
-                <v-icon
+                    color="success"
                     dark
-                    left
-                    >
-                    mdi-checkbox-marked-circle
-                </v-icon>
-                Add
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    Add Item
                 </v-btn>
-            </v-card-actions>
-        </v-card>
-    </v-dialog>
+            </template>
+            <v-card class="pa-6">
+                <v-form
+                    ref="newsForm"
+                    v-model="isValid"
+                >
+                    <v-text-field
+                        v-model="newsItem.title"
+                        v-bind="inputBindings"
+                        label="Title"
+                        :rules="rules"
+                    />
+                    <v-textarea
+                        v-model="newsItem.body"
+                        v-bind="inputBindings"
+                        name="input-7-4"
+                        label="Content"
+                        :rules="rules"
+                    />
+                    <v-select
+                        v-model="newsItem.catId"
+                        :items="items"
+                        item-text="title"
+                        item-value="catId"
+                        label="Category"
+                        v-bind="inputBindings"
+                        :rules="rules"
+                    />
+                </v-form>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="purple darken-2"
+                        text
+                        @click="dialog = false"
+                    >
+                        Cancel
+                    </v-btn>
+                    <v-btn
+                        color="primary"
+                        text
+                        @click="onAdd"
+                    >
+                        <v-icon
+                            dark
+                            left
+                        >
+                            mdi-checkbox-marked-circle
+                        </v-icon>
+                        {{buttonText}}
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
     </v-row>
 </template>
 
 <script>
+const requiredField = v => !!v && !!v.length || 'This field is required'
+
 export default {
-    name: "AddItem",
-    data () {
-        return {
-        dialog: false,
-        items: [
-            {
-                catId: "1",
-                title: "Main"
-            },
-            {
-                catId: "2",
-                title: "War",
-            },
-            {
-                catId: "3",
-                title: "Politic",
-            },
-            {
-                catId: "4",
-                title: "Economy",
-            },
-            {
-                catId: "5",
-                title: "Science",
+    name: 'AddItem',
+    computed: {
+        buttonText() {
+            console.log('buttonText')
+
+            const isCreating = Object.keys(this.newsItem).length === 0
+
+            return isCreating
+                ? 'Create'
+                : 'Update'
+        },
+        inputBindings() {
+            return {
+                outlined: false,
+                dense: true
             }
-        ],
-        titleValue: "",
-        contentValue: "",
-        categoryValue: "",
-        news: [],
+        }
+    },
+    watch: {
+        dialog(flag) {
+            if(!flag) {
+                this.onBack()
+            }
+        }
+    },
+    data() {
+        return {
+            dialog: false,
+            items: [
+                {
+                    catId: '1',
+                    title: 'Main'
+                },
+                {
+                    catId: '2',
+                    title: 'War'
+                },
+                {
+                    catId: '3',
+                    title: 'Politic'
+                },
+                {
+                    catId: '4',
+                    title: 'Economy'
+                },
+                {
+                    catId: '5',
+                    title: 'Science'
+                }
+            ],
+            news: [
+                {
+                    title: 'new Title',
+                    body: 'new body',
+                    catId: '3'
+                }
+            ],
+            newsItem: {},
+            isValid: true,
+            rules: [requiredField]
         }
     },
     methods: {
-        onTitleChange(value){
-            this.titleValue = value
+        onEdit() {
+            const item = this.news[0]
+            this.newsItem = Object.assign({}, item)
+            this.dialog = true
         },
-        onContentChange(value){
-            this.contentValue = value
+        onBack() {
+            this.newsItem = {}
+            this.$refs.newsForm.reset()
         },
-        onCategoryChange(value){
-            this.categoryValue = value
-        },
-        onBack(){
-            this.dialog = false,
-            this.titleValue = "",
-            this.contentValue = "",
-            this.categoryValue = ""
-        },
-        onAdd(){
-            this.news.push({
-                newsId: this.news.length + 1,
-                title: this.titleValue,
-                body: this.contentValue,
-                categoryId: this. categoryValue,
+        onAdd() {
+            if(!this.$refs.newsForm.validate()) {
+                return
+            }
 
-            });
-           this.onBack()
-                
+            this.news.push(this.newsItem)
+
             console.log('this.news', this.news)
-        },
-       
+        }
+
 
     }
 }
